e60e939b020e46d310670ac3bde96e0237f9e070
Jethro Swanson <jethro.swanson@gmail.com>
2023-02-25 16:43:21 -0600

    Fixed horrible error

--- list_site/shopping_list/models.py
+++ list_site/shopping_list/models.py
@@ -3,38 +3,26 @@ from django.urls import reverse
 
 # Create your models here.
 class List(models.Model):
-    title = models.CharField(max_length=100,null=True)
+    list_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
-
-    def get_absolute_url(self):
-        return reverse('list_detail', args=[str(self.id)])
-
-    def get_pk(self):
-        return str(self.id)
 
     def __str__(self):
         return self.title
     
 
 class Item(models.Model):
-    itemName = models.CharField(max_length=100,null=True)
+    item_id = models.AutoField(primary_key=True)
+    itemName = models.CharField(max_length=100)
     description = models.TextField()
-    priority = models.CharField(max_length=100, null=True)
+    priority = models.CharField(max_length=100)
     date_modified = models.DateField(auto_now=True)
-    list = models.ForeignKey('List',related_name='items',on_delete=models.CASCADE, null= True)
+    #list = models.ForeignKey('List',related_name='items',on_delete=models.CASCADE, null= True)
 
     class Meta:
         ordering = ['-date_modified']
 
     def __str__(self):
         return self.itemName
-
-class ShopList(models.Model):
-    title = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse("shop_detail", kwargs={"pk": self.pk})
+        
     
--- list_site/shopping_list/urls.py
+++ list_site/shopping_list/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('test/',views.test,name = 'test'),
-    path('yourlist/', views.ListListView.as_view(), name="yourlist"),
-    path('yourlist/<int:pk>/', views.ListDetailView.as_view(), name = 'list_detail')
+    path('yourlist/', views.ListListView.as_view(), name='yourlist'),
+    path('yourlist/<int:pk>', views.ListDetailView.as_view(), name = 'list_detail')
 ]
