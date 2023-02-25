db202ac4f7f43fa8075e90998b8bd6af2c85ebe1
Jethro Swanson <jethro.swanson@gmail.com>
2023-02-25 15:39:46 -0600

    Created user templates for log-in functionality

--- list_site/list_site/urls.py
+++ list_site/list_site/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/', include('shopping_list.urls')),
     path('', RedirectView.as_view(url='list/')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
