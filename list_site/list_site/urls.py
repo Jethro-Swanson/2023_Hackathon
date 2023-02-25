d0ef06d1e9e9fcc0f38393bb54514157b959e584
Jethro Swanson <jethro.swanson@gmail.com>
2023-02-25 09:57:06 -0600

    Added Django autogenerrated sub-app

--- list_site/list_site/urls.py
+++ list_site/list_site/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.urls import include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('list/', include('shopping_list.urls')),
+    path('', RedirectView.as_view(url='list/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
